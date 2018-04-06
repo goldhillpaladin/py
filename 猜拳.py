1b26bde71e1cbcf0ff401082eabc8c64139cd6ad
goldhillpaladin <qiansheng870601@163.com>
2018-04-06 17:25:58 +0800

    fix bug

--- 猜拳.py
+++ 猜拳.py
@@ -9,17 +9,18 @@ while True:
 		ind = '石头'
 	elif s == 2:
 		ind = '剪刀'
-	else s == 3:
+	else:
 		ind = '布'
-	m = raw_input('输入 石头、剪子、布,输入"end"结束游戏:')
-	blist = ['石头', '剪子', '布']
+	m = raw_input('输入 石头、剪刀、布, 输入"end"结束游戏:\n')
+	blist = ['石头', '剪刀', '布']
 	if m == 'end':
 		print "\n游戏退出中..."
+		break;
 	elif (m not in blist):
 		print '输入错误，请重新输入！'
 	elif m == ind:
 		print "电脑出了： " + ind + "，平局！"
-	elif (m == '石头' and ind =='剪子') or (m == '剪子' and ind =='布') or (m == '布' and ind =='石头'):
+	elif (m == '石头' and ind =='剪刀') or (m == '剪刀' and ind =='布') or (m == '布' and ind =='石头'):
 		print "电脑出了： " + ind +"，你赢了！"
 	else:
 		print "电脑出了： " + ind +"，你输了！"
